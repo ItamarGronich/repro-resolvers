13eada4d2a4206f3c083f18856f06287f0ab1b5e
ItamarGronich <itamar.gronich@fiverr.com>
2025-08-24 17:54:52 +0300

    yarn

--- test.js
+++ test.js
@@ -7,11 +7,11 @@ const execAsync = promisify(exec);
 
 const packageManager = {
   getPackageManagerDetails: async () => ({
-    name: "npm",
-    version: (await runCommand("npm --version")).stdout.trim(),
+    name: "yarn",
+    version: (await runCommand("yarn --version")).stdout.trim(),
   }),
-  install: () => runCommand("npm ci"),
-  build: () => runCommand("npm run build"),
+  install: () => runCommand("yarn install --immutable"),
+  build: () => runCommand("yarn run build"),
 };
 
 async function runCommand(cmd) {
