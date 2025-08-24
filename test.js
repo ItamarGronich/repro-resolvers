48fa4b309826dd55fad014a703a0f4b2806b5f8a
ItamarGronich <itamar.gronich@fiverr.com>
2025-08-24 17:39:51 +0300

    add test

--- test.js
+++ test.js
@@ -5,35 +5,54 @@ import { promisify } from "node:util";
 
 const execAsync = promisify(exec);
 
+const packageManager = {
+  name: "npm",
+  install: () => runCommand("npm ci"),
+  build: () => runCommand("npm run build"),
+};
+
+async function runCommand(cmd) {
+  try {
+    return await execAsync(cmd, { stdio: "inherit" });
+  } catch (error) {
+    throw new Error(
+      `Error: the command: ${cmd} exited with exit code ${
+        error.code
+      }.\n======= STDOUT =======:\n${
+        error.stdout || "<none>"
+      }\n======= STDERR =======:\n${error.stderr || "<none>"}\n`
+    );
+  }
+}
+
 function log(cmd) {
-  console.log(`========== ${cmd} ==========`);
+  console.log(`- ${cmd}`);
 }
 
 async function clean() {
-  log("clean");
+  log("cleaning...");
   await Promise.all([
     rm(join(process.cwd(), "dist"), { recursive: true, force: true }),
     rm(join(process.cwd(), "node_modules"), { recursive: true, force: true }),
   ]);
-  log("cleaned");
 }
 
 async function install() {
-  log("install");
-  await execAsync("npm ci");
-  log("installed");
+  log(`installing...`);
+  await packageManager.install();
 }
 
 async function build() {
-  log("build");
-  await execAsync("npm run build");
-  log("built");
+  log(`building...`);
+  await packageManager.build();
 }
 
 async function test() {
+  log(`testing package manager: ${packageManager.name}`);
   await clean();
   await install();
   await build();
+  log("done ✅");
 }
 
 test();
